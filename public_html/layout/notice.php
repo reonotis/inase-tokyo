25a3e4233a068eced64f89e555bf2e5712d0dfb1
REONOTIS\fujisawa <fujisawa@reonotis.jp>
2022-04-24 15:19:39 +0900

    画像のパス修正

--- public_html/layout/notice.php
+++ public_html/layout/notice.php
@@ -4,7 +4,7 @@
     <div class='notice-area' >
         <div class='flex notice-content-row' >
             <div class='notice-content-image' >
-                <img src='../img/photo/lunch_01.png' >
+                <img src='./img/photo/lunch_01.png' >
             </div>
             <div class='notice-contents' >
                 <div class='notice-content-wrapper' >ランチメニューのご紹介<span class="notice-content-support" >テイクアウト承ります（デリバリー準備中）</span></div>
