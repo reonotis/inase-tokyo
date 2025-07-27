b7b6d32efd4ede7f0a1ae9d078b95d4e26ff7042
REONOTIS\fujisawa <fujisawa@reonotis.jp>
2025-07-27 13:54:46 +0900

    閉店のCSS修正

--- index.php
+++ index.php
@@ -5,7 +5,9 @@
         <div class="background-black" >
             <?php include './layout/header.php'; ?>
             <div class="top-contents-area" id="top">
-                <iframe src="./file/closed_notice.pdf">
+                <iframe
+                        src="https://docs.google.com/gview?embedded=true&url=https://www.inase-tokyo.com/file/closed_notice.pdf"
+                        style=" border:none;">
                 </iframe>
             </div>
         </div>
