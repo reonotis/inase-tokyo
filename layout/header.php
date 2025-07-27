dc6c8df9607779811123c6db008c8c99f6c13ee7
REONOTIS\fujisawa <fujisawa@reonotis.jp>
2025-07-27 13:11:33 +0900

    閉店のPDFを標示

--- layout/header.php
+++ layout/header.php
@@ -13,28 +13,11 @@
         <!-- <div class='side-menu' id='side-menu' style='display:none;' > -->
             <div class='menu-mask' id='menu-mask' style='display:none;'  ></div>
             <div class='menu-bar' id='menu-bar' >
-                <ul class="side-menu">
-                    <li><a href='#top'>TOP</a></li>
-                    <li><a href='#greeting'>ご挨拶</a></li>
-                    <li><a href='#content_1'>こだわり</a></li>
-                    <li><a href='#notice'>お知らせ</a></li>
-                    <li><a href='#access-area'>お店のご案内</a></li>
-                    <li><a href='#access'>ご予約</a></li>
-                    <li><a href='/en'>English</a></li>
-                </ul>
             </div>
         <!-- </div> -->
-        <div class='hamburger' id='hamburger' ></div>
         東麻布 割烹 鯔背 -INASE-
 
         <div class='sns-area flex' >
-            <?php foreach($snsList as $sns){ ?>
-                <a href='<?= $sns['url'] ?>' target="_blank" class='sns-contents flex' >
-                    <span class="<?= $sns['name'] ?>" font-awesome="<?= $sns['name'] ?>" ></span>
-                    <span class="sp-hidden" ><?= $sns['name'] ?></span>
-                </a>
-            <?php }?>
-            <a href="/en" class="sns-contents flex language-change-btn" >English</a>
         </div>
     </div>
 
